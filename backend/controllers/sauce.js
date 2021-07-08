a376a265d4cc6d8ab596ffdabae24833b318cacf
Fabien R <79097567+Zheyn@users.noreply.github.com>
2021-07-08 11:02:27 +0200

    Ajout like et dislike

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -6,7 +6,11 @@ exports.createSauce = (req, res, next) => {
     delete sauceObject._id;
     const sauce = new Sauce({
       ...sauceObject,
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+      likes: 0,
+      dislikes: 0,
+      usersLiked: [],
+      usersDisliked: []
     });
     sauce.save()
       .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
@@ -47,4 +51,102 @@ exports.getAllSauce = (req, res, next) => {
     Sauce.find()
       .then(sauces => res.status(200).json(sauces))
       .catch(error => res.status(400).json({ error }));
+}
+
+exports.like = (req, res, next) => {
+  // Pour la route READ = Ajout/suppression d'un like / dislike à une sauce
+  // Like présent dans le body
+  let like = req.body.like
+  // On prend le userID
+  let userId = req.body.userId
+  // On prend l'id de la sauce
+  let sauceId = req.params.id
+
+  if (like === 1) { // Si il s'agit d'un like
+    Sauce.updateOne({
+        _id: sauceId
+      }, {
+        // On push l'utilisateur et on incrémente le compteur de 1
+        $push: {
+          usersLiked: userId
+        },
+        $inc: {
+          likes: +1
+        }, // On incrémente de 1
+      })
+      .then(() => res.status(200).json({
+        message: 'j\'aime ajouté !'
+      }))
+      .catch((error) => res.status(400).json({
+        error
+      }))
+  }
+  if (like === -1) {
+    Sauce.updateOne( // S'il s'agit d'un dislike
+        {
+          _id: sauceId
+        }, {
+          $push: {
+            usersDisliked: userId
+          },
+          $inc: {
+            dislikes: +1
+          }, // On incrémente de 1
+        }
+      )
+      .then(() => {
+        res.status(200).json({
+          message: 'Dislike ajouté !'
+        })
+      })
+      .catch((error) => res.status(400).json({
+        error
+      }))
+  }
+  if (like === 0) { // Si il s'agit d'annuler un like ou un dislike
+    Sauce.findOne({
+        _id: sauceId
+      })
+      .then((sauce) => {
+        if (sauce.usersLiked.includes(userId)) { // Si il s'agit d'annuler un like
+          Sauce.updateOne({
+              _id: sauceId
+            }, {
+              $pull: {
+                usersLiked: userId
+              },
+              $inc: {
+                likes: -1
+              }, // On incrémente de -1
+            })
+            .then(() => res.status(200).json({
+              message: 'Like retiré !'
+            }))
+            .catch((error) => res.status(400).json({
+              error
+            }))
+        }
+        if (sauce.usersDisliked.includes(userId)) { // Si il s'agit d'annuler un dislike
+          Sauce.updateOne({
+              _id: sauceId
+            }, {
+              $pull: {
+                usersDisliked: userId
+              },
+              $inc: {
+                dislikes: -1
+              }, // On incrémente de -1
+            })
+            .then(() => res.status(200).json({
+              message: 'Dislike retiré !'
+            }))
+            .catch((error) => res.status(400).json({
+              error
+            }))
+        }
+      })
+      .catch((error) => res.status(404).json({
+        error
+      }))
+  }
 }
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -14,5 +14,8 @@ router.get('/:id', auth, sauceCtrl.getOneSauce);
   
 router.get('/', auth, sauceCtrl.getAllSauce);
 
+// Route qui permet de gérer les likes des sauces
+// Définit le statut "j'aime" pour userID fourni. Si j'aime = 1,l'utilisateur aime la sauce. Si j'aime = 0,l'utilisateur annule ce qu'il aime ou ce qu'il n'aime pas. Si j'aime =-1, l'utilisateur n'aime pas la sauce.L'identifiant de l'utilisateur doit être ajouté ou supprimé du tableau approprié, engardant une trace de ses préférences et en l'empêchant d'aimer ou de ne pas aimer la même sauce plusieurs fois. Nombre total de "j'aime" et de "je n'aime pas" à mettre à jour avec chaque "j'aime".
+router.post('/:id/like', auth, sauceCtrl.like)
 
 module.exports = router;
